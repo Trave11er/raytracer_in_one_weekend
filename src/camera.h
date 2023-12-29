17342424d381f3037061253b8addef5fc74bf012
Gleb Siroki <5238128+Trave11er@users.noreply.github.com>
2023-12-29 11:43:17 +0200

    Add depth of field

--- src/camera.h
+++ src/camera.h
@@ -21,6 +21,9 @@ class camera {
     point3 lookat   = point3(0,0,0);   // Point camera is looking at
     vec3   vup      = vec3(0,1,0);     // Camera-relative "up" direction
 
+    double defocus_angle = 0;  // Variation angle of rays through each pixel
+    double focus_dist = 10;    // Distance from camera lookfrom point to plane of perfect focus
+
     void render(const hittable_list& world) {
         initialize();
 
@@ -48,6 +51,8 @@ class camera {
     vec3   pixel_delta_u;  // Offset to pixel to the right
     vec3   pixel_delta_v;  // Offset to pixel below
     vec3   u, v, w;
+    vec3   defocus_disk_u;  // Defocus disk horizontal radius
+    vec3   defocus_disk_v;  // Defocus disk vertical radius
 
     void initialize() {
         // Calculate the image height, and ensure that it's at least 1.
@@ -56,9 +61,10 @@ class camera {
 
         camera_center = lookfrom;
 
-        auto focal_length = (lookfrom - lookat).length();
         auto vfov_rad = degrees_to_radians(vfov);
-        auto viewport_height = 2 * focal_length * tan(vfov_rad / 2);
+        //auto focal_length = (lookfrom - lookat).length();
+        //auto viewport_height = 2 * focal_length * tan(vfov_rad / 2);
+        auto viewport_height = 2 * focus_dist * tan(vfov_rad / 2);
         auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
 
 
@@ -76,14 +82,21 @@ class camera {
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate the location of the upper left pixel.
-        auto viewport_upper_left = camera_center - focal_length * w - viewport_u/2 - viewport_v/2;
+        //auto viewport_upper_left = camera_center - focal_length * w - viewport_u/2 - viewport_v/2;
+        auto viewport_upper_left = camera_center - focus_dist * w - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        // Calculate the camera defocus disk basis vectors.
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     ray get_ray(int i, int j) const {
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
         auto pixel_sample = pixel_center + pixel_sample_square();
-        auto ray_origin = camera_center;
+        //auto ray_origin = camera_center;
+        auto ray_origin = (defocus_angle <= 0) ? camera_center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
         return ray(ray_origin, ray_direction);
     }
@@ -95,6 +108,11 @@ class camera {
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
+    point3 defocus_disk_sample() const {
+        // Returns a random point in the camera defocus disk.
+        auto p = random_in_unit_disk();
+        return camera_center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+    }
 
     color ray_color(const ray& r, int depth, const hittable_list& world) {
         hit_record rec;
