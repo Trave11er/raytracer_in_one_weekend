94ab50d6b8d4497fcbd123452cbc6b734f7f2b8c
Gleb Siroki <5238128+Trave11er@users.noreply.github.com>
2023-12-29 10:42:42 +0200

    Add total internal reflection

--- src/hittable.h
+++ src/hittable.h
@@ -92,14 +92,31 @@ class dielectric : public material {
 
         double refraction_ratio = front_face ? (1.0/ir) : ir;
         vec3 unit_direction = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_direction, scatter_normal, refraction_ratio);
+        double cos_theta = fmin(dot(-unit_direction, scatter_normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
 
-        scattered = ray(rec.p, refracted);
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+            direction = reflect(unit_direction, scatter_normal);
+        else
+            direction = refract(unit_direction, scatter_normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
+
         return true;
     }
 
   private:
     double ir; // Index of Refraction
+
+    static double reflectance(double cosine, double ref_idx) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1-ref_idx) / (1+ref_idx);
+        r0 = r0*r0;
+        return r0 + (1-r0)*pow((1 - cosine),5);
+    }
 };
 
 #endif
